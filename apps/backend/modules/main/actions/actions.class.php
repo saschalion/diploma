62cd0b0b4d740d3a1da60dc7c90966b7263dd226
Alex <saschalion@gmail.com>
2012-02-07 16:57:02 +0700

    Create project

--- apps/backend/modules/main/actions/actions.class.php
+++ apps/backend/modules/main/actions/actions.class.php
@@ -4,13 +4,34 @@ require_once dirname(__FILE__).'/../lib/mainGeneratorConfiguration.class.php';
 require_once dirname(__FILE__).'/../lib/mainGeneratorHelper.class.php';
 
 /**
- * main actions.
- *
- * @package    forest
- * @subpackage main
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
- */
+* job actions.
+*
+* @package jobeet
+* @subpackage job
+* @author Your name here
+* @version SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+*/
 class mainActions extends autoMainActions
 {
+    public function preExecute()
+        {
+            parent::preExecute();
+        }
+
+          public function executeShow(sfWebRequest $request)
+  {
+    $this->forest_main = Doctrine_Core::getTable('ForestMain')->find(array($request->getParameter('id')));
+    $this->forward404Unless($this->forest_main);
+    $this->setLayout('layout_not');
+    $this->getResponse()->addStylesheet('print.css', 'last', array('media' => 'print'));
+    $this->getResponse()->addStylesheet('show.css', 'last');
+  }
+  
+            public function executeChart(sfWebRequest $request)
+  {
+    $this->forest_main = Doctrine_Core::getTable('ForestMain')->find(array($request->getParameter('id')));
+    $this->setLayout('layout_not');
+    $this->getResponse()->addStylesheet('print.css', 'last', array('media' => 'print'));
+    $this->getResponse()->addStylesheet('show.css', 'last');
+  }  
 }
