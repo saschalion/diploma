90fdf4b9c808041c0b10f354cd4399970bdf3d33
Alex <saschalion@gmail.com>
2012-03-15 21:07:08 +0700

    Graph has been made

--- data/html/js/reports.js
+++ data/html/js/reports.js
@@ -6,112 +6,34 @@ function drawChart() {
   
     var data = new google.visualization.DataTable();
 
-    data.addColumn('string', 'Name');
-    data.addColumn('number', 'Activity');
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Y');
     
-    data.addRows([
-        ['Jeff Jones', 0.45],
-        ['John Baker', 55],
-        ['Jen Yang', 0.85], 
-        ['Mike Gertz', 23.45],
-        ['Victoria Saint', 45]
-    ]);
+    function V(x, A, B, C, D, E, F) {
+      return Math.pow(x, 8) / 8
+        + (A / 6) * Math.pow(x, 6)
+        + (B / 5) * Math.pow(x, 5)
+        + (C / 4) * Math.pow(x, 4)
+        + (D / 3) * Math.pow(x, 3)
+        + (E / 2) * Math.pow(x, 2)
+        + F * x
+      ;
+    };
+    
 
+        for(F = -100; F <= 100; F++)
+            for(D = -100; D <= 100; D++)
+                data.addRows([[F, V(F, 0, 0, 50, 4, -24, D)]]);
+    
     var options = {
         'title':'Top 5 Mentees in terms of mentoring efficiency last one week',
-        'width':250,
-        'height':140
+        'width':450,
+        'height':240
     };
 
     chart = new google.visualization.LineChart(document.getElementById('chart_div2'));
     chart.draw(data, options);
 }
-
-google.setOnLoadCallback(drawChartSecond);
-
-function drawChartSecond() {
-    var dataSecond = new google.visualization.DataTable();
-    dataSecond.addColumn('string', 'Name');
-    dataSecond.addColumn('number', 'Activity');
-    dataSecond.addRows([
-        ['Jeff Jones', 39],
-        ['John Baker', 12],
-        ['Jen Yang', 14], 
-        ['Mike Gertz', 1],
-        ['Victoria Saint', 2]
-    ]);
-
-    var options = {
-        'title':'Top 5 Mentees in terms of mentoring activity in last week',
-        'width':250,
-        'height':140
-    };
-
-    chartSecond = new google.visualization.PieChart(document.getElementById('chart_div'));
-    chartSecond.draw(dataSecond, options);
-}
-
-google.setOnLoadCallback(drawChartThird);
-
-function drawChartThird() {
-    var dataThird = new google.visualization.DataTable();
-    dataThird.addColumn('string', 'Month');
-    dataThird.addColumn('number', 'Overall Efficiency Percentage');
-    dataThird.addRows([
-        ['Jan', 45],
-        ['Feb', 50],
-        ['Mar', 52], 
-        ['Apr', 52],
-        ['May', 60],
-        ['June', 65],
-        ['July', 65],
-        ['Aug', 75], 
-        ['Sep', 78],
-        ['Oct', 78],
-        ['Nov', 80], 
-        ['Dec', 89]
-    ]);
-
-    var options = {
-        'title':'Mentoring Efficiency at Program Level',
-        'width':250,
-        'height':140
-    };
-
-    chartThird = new google.visualization.BarChart(document.getElementById('chart_div1'));
-    chartThird.draw(dataThird, options);
-}
-
-google.setOnLoadCallback(drawChartFouth);
-
-function drawChartFouth() {
-    var dataFouth = new google.visualization.DataTable();
-    dataFouth.addColumn('string', 'Month');
-    dataFouth.addColumn('number', 'Average Efficiency');
-    dataFouth.addColumn('number', 'Joe Giglio');
-    dataFouth.addColumn('number', 'John White');
-    dataFouth.addColumn('number', 'John Wright');
-    dataFouth.addColumn('number', 'Jeff Dujon');
-    dataFouth.addColumn('number', 'Mike Kaiser');
-
-    dataFouth.addRows([
-        ['July', 65, 20, 25,30,33,34],
-        ['Aug', 75, 20, 25,30,33,34], 
-        ['Sep', 78,20, 30,35,35,35],
-        ['Oct', 78,25, 30,35,35,35],
-        ['Nov', 80,25,30,40,35,35], 
-        ['Dec', 89,30,35,40,35,35]
-    ]);
-
-    var options = {
-        width: 250, height: 140,
-        title: 'Top 5 Mentees Needing Improvement'
-    };
-
-    chartFouth = new google.visualization.LineChart(document.getElementById('chart_div3'));
-    chartFouth.draw(dataFouth, options);
-}      
-
 
 $(function(){
   $('.edit-profile-form input[type="text"]').keyup(function() {
